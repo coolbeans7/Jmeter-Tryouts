fe5430a46f483b13268df4e4c577bde66fb67c2c
agerho000 <Andrew_Gerhold@sv.comcast.com>
2013-11-27 13:49:07 -0800

    modified launchbees

--- launchbees.sh
+++ launchbees.sh
@@ -7,4 +7,4 @@
 #dev12 - 172.20.3.71
 #dev13 - 172.20.3.70
 #dev14 -
-./apache-jmeter-2.9/bin/jmeter -n -t tests/seeitload01.jmx -R dev08,dev12,dev13,dev14,dev65,dev67
+./apache-jmeter-2.9/bin/jmeter -n -t tests/seeitload01.jmx -R dev08,dev12,dev13,dev14,dev65,dev67 -l results.csv
